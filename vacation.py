9061b0a65c56b1476add84a4efbe58e788d1094c
emiller213 <emiller213@gmail.com>
2018-05-25 14:07:49 -0400

    Initial commit of vacation.py
    initial commit

---
+++ vacation.py
@@ -0,0 +1,16 @@
+def hotel_cost(nights):
+  #If Hotel equals $140 / night
+  return 140 * nights
+
+def plane_ride_cost(city):
+  if city == "Charlotte":
+    return 183
+  elif city == "Tampa":
+    return 220
+  elif city == "Pittsburgh":
+    return 222
+  elif city == "Los Angeles":
+    reutrn 475
+  else:
+    return "unknown"
+    
